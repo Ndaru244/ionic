6b67b73fa1dda61763bf6e38bd80b94ab7bf041a
Ndaru244 <kring08569@gmail.com>
2021-10-27 23:30:56 +0700

    updated source

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'tugas-8',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+export default config;
